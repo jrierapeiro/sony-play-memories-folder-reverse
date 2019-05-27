f81bd0413a1884bc03bafd28b9a5bee482ca44a1
jariepei <jariepei@gmail.com>
2019-05-27 18:12:12 +0100

    Add folder name as argument

--- folder_rename_script.js
+++ folder_rename_script.js
@@ -1,29 +1,44 @@
 const fs = require('fs')
 const path = require('path')
 
-const folderName = 'F:\\Photos\\PM'
+const defaultFolderName = 'F:\\Photos\\PM'
 
-try {
-  if (!fs.existsSync(folderName)){
-    fs.mkdirSync(folderName)
+const renameFolders = (args) => {
+  console.log('Args: ', args);
+
+  let folderName = defaultFolderName;
+  // Args 0 and 1 are node + program.js
+  if (args && args.length > 2) {
+    folderName = args[2];
   }
 
-  fs.readdirSync(folderName).map(fileName => {
-    const parts = fileName.split('-');
-    if(parts.length === 3){     
-      if(parts[0].length === 2){
-        const newFolderName = parts.reverse().join('-');
-        console.log(fileName, newFolderName);
-        console.log('Using old format', parts,  fileName);
-        try {
-          fs.renameSync(path.join(folderName, fileName), path.join(folderName, newFolderName))
-        } catch (err) {
-          console.error(err)
-        }
-      }      
+  console.log('Working on folder: ', folderName);
+  try {
+    if (!fs.existsSync(folderName)) {
+      fs.mkdirSync(folderName)
     }
-  });
 
-} catch (err) {
-  console.error(err)
-}
+    fs.readdirSync(folderName).map(fileName => {
+      const parts = fileName.split('-');
+      if (parts.length === 3) {
+        // Old Format MM-DD-YYYY
+        if (parts[0].length === 2) {
+          const newFolderName = parts.reverse().join('-');
+          console.log('Renaming folder:', fileName, newFolderName);
+          try {
+            fs.renameSync(path.join(folderName, fileName), path.join(folderName, newFolderName))
+          } catch (err) {
+            console.error(err)
+          }
+        } else {
+          console.log('Folder already using new format', fileName);
+        }
+      }
+    });
+  } catch (err) {
+    console.error(err)
+  }
+};
+
+renameFolders(process.argv);
+
